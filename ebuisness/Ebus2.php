7d1816978757705a5621663b792b7b503126b253
Victor Bourke <117461862@umail.ucc.ie>
2018-02-09 10:40:11 +0000

    Ebus2

---
+++ ebuisness/Ebus2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Enter Details</title>
+        
+        <!--jQuery-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    </head>
+    <body>
+        <h4>Please enter your payment details</h4>
+        
+        
+            <form action="Ebus3.php" method="POST">
+
+                    <label for="user_pin">PIN</label>
+                    <input type="password" id="user_pin" placeholder="Card PIN" maxlength="4">
+
+                <button type="submit" id="btnPurchase" disabled>Proceed with Purchase</button>
+              
+            </form>
+            
+            <br/>
+            <button onClick="validateDetails()">Validate</button>
+            
+        </div>
+        
+        <script type="text/javascript" src="ebus2_validator.js"></script>
+    </body>
+</html>
