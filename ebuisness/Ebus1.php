5934d265aa988ce10368eff2f14d2422fc34fd8b
Victor Bourke <117461862@umail.ucc.ie>
2018-02-18 23:17:52 +0000

    Carousel for Homepage

--- ebuisness/Ebus1.php
+++ ebuisness/Ebus1.php
@@ -1,57 +1,58 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Select Products</title>
+        <title>Select Product</title>
         
-        <!--JQuery-->
-        <script src="https//ajax.googleeapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <!--jQuery-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script type="text/javascript" src="cost_calc.js"></script>
-        
+
     </head>
+    
     <body>
         
-        <h4>Select a Product</h4>
-        
-        <br/>
-        
-        <form method="POST" action="Ebus2.php">
-            
-            <label for="salesforce">
-                <input type="radio" id="salesforce" name="product" checked onClick="disablebtnProceed()"/>
-                Salesforce @ $100
-            </label>
+            <h4>Select a Product</h4>
             
             <br/>
             
-            <label for="aws">
+            <form method="POST" action="Ebus2.php">
+              
+              <label for="salesforce">
+                <input type="radio" id="salesforce" name="product" checked onClick="disablebtnProceed()"/>
+                SalesForce @ $100
+              </label>
+              
+              <br/>
+              
+              <label for="aws">
                 <input type="radio" id="aws" name="product" onClick="disablebtnProceed()"/>
                 AWS @ $300
-            </label>
-            
-            <br/>
-            <br/>
-            
-            <label for="subtotal">
+              </label>
+              
+              <br/>
+              <br/>
+              
+              <label for="subtotal">
                 Sub Total
                 <input type="text" id="subtotal" value="0.00" readonly/>
-            </label>
-            
-            <br/>
-            
-            <label for="total">
+              </label>
+              
+              <br/>
+              
+              <label for="total">
                 Total
-                <input type="text" id="total" value="0.00" readonly/>
-            </label>
+                <input type="hidden" id="total" name="total" value="0.00" readonly/>
+              </label>
+    
+              <br/>
+              
+              <button type="submit" id="btnProceed" disabled>Add to Shopping Cart</button>
+            
+            </form>
             
             <br/>
-            
-            <button type="submit" id="btnProceed" disabled>Add to Shopping Cart</button>
-            
-        </form>
-        
-        <br/>
-        <button onClick="calcSub()">Calculate Cost</button>
-        <a role="button" href="Ebus1.php">Clear Choice</a>    
+            <button onClick="calcSub()">Calculate Cost</button>
+            <a role="button" href="Ebus1.php">Clear Choice</a>
     
     </body>
 </html>
--- ebuisness/Ebus2.php
+++ ebuisness/Ebus2.php
@@ -1,3 +1,7 @@
+<?php
+// Start the session
+session_start();
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,6 +9,7 @@
         
         <!--jQuery-->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script type="text/javascript" src="ebus2_validator.js"></script>
     </head>
     <body>
         <h4>Please enter your payment details</h4>
@@ -13,6 +18,7 @@
             <form action="Ebus3.php" method="POST">
 
                     <label for="user_pin">PIN</label>
+                    
                     <input type="password" id="user_pin" placeholder="Card PIN" maxlength="4">
 
                 <button type="submit" id="btnPurchase" disabled>Proceed with Purchase</button>
@@ -22,8 +28,10 @@
             <br/>
             <button onClick="validateDetails()">Validate</button>
             
-        </div>
+            <?php
+            // Set session variables
+            $_SESSION["total"] = $_POST["total"];
+            ?>
         
-        <script type="text/javascript" src="ebus2_validator.js"></script>
     </body>
 </html>
