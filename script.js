6d15215d40fbf4ca8e92d24d2abef1ba1cb6c0da
Victor Bourke <117461862@umail.ucc.ie>
2018-02-21 23:17:03 +0000

    Trying to make footer

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  
-  $('.logo').hover(
-       function(){ $(this).attr('class', 'logo logo--hover') },
-       function(){ $(this).attr('class', 'logo') }
-)
-  
-});
