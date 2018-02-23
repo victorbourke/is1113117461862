35e304c975735d06bc8768a995a74ce2c9138b73
Victor Bourke <117461862@umail.ucc.ie>
2018-02-23 10:50:05 +0000

    Adding social icons to footer and pasting style across website

--- ebuisness/Ebus1.php
+++ ebuisness/Ebus1.php
@@ -9,7 +9,117 @@
 
     </head>
     
+    
+    
     <body>
+      
+      <style>
+        
+      body {
+      	margin: 0;
+      	background: #2F2FA2;
+      	font-family: 'Work Sans', sans-serif;
+      	font-weight: 400;
+      }
+      
+    .container {
+      	width: 80%;
+      	margin: 0 auto;
+      }
+      
+      header {
+        background: #2F2FA2;
+      }
+      
+      header::after {
+        content: '';
+        display: table;
+        clear: both;
+      }
+      
+       .logo {
+        float: left;
+        padding-top: 10px;
+      }
+      
+      nav {
+        float: right;
+      }
+      
+      nav ul {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+      }
+      
+      nav li {
+        display: inline-block;
+        margin-left: 70px;
+        padding-top: 23px;
+      
+        position: relative;
+      }
+      
+      nav a {
+        color: #ffffff;
+        text-decoration: none;
+        text-transform: uppercase;
+        font-size: 17px;
+      }
+      
+      nav a:hover {
+        color: #F64C72;
+      }
+      
+      nav a::before {
+        content: '';
+        display: block;
+        height: 5px;
+        background-color: #F64C72;
+      
+        position: absolute;
+        top: 0;
+        width: 0%;
+      
+        transition: all ease-in-out 250ms;
+      }
+      
+      nav a:hover::before {
+        width: 100%;
+      }
+      
+
+
+      
+      
+</style>
+        
+        
+        <header>
+    
+    <div class="container">
+      
+      <img src="/logoyup.png" alt="logo" class"logo">
+      
+      <h1 class="logo"></h1>
+      
+      <nav>
+        
+        <ul>
+          <li><a href="/homepage.html">Home</a></li>
+          <li><a href="/cv_page1.html">CV</a></li>
+          <li><a href="/Interests/sports.html">Interest</a></li>
+          <li><a href="/ebuisness/Ebus1.php">EBuisness</a></li>
+        </ul>
+      </nav>
+      
+    </div>
+    
+  </header>
+  
+        
+        
+        
         
             <h4>Select a Product</h4>
             
